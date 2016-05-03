d099dbea1b2dcb091b884d62f0faffa2f02cac52
Anthony <nixegend@gmail.com>
2016-05-04 01:40:09 +0300

    command pattern

--- patterns/command.js
+++ patterns/command.js
@@ -21,3 +21,50 @@ var carManager = {
 };
 
 carManager.execute("buyVehicle", "Ford Escort", "453543");
+
+// ====== Another implementation of explaining how it can work with other classes ======
+
+function plus(x, y) { return x + y; };
+function minus(x, y) { return x - y; };
+
+function Command(execute, undo, value) {
+    this.execute = execute;
+    this.undo = undo;
+    this.value = value;
+}
+
+function PlusCommand(value) {
+    return new Command(plus, minus, value);
+};
+
+function MinusCommand(value) {
+    return new Command(minus, plus, value);
+};
+
+function Calc() {
+    this.current = 0;
+    this.commands = []; // the story of commands
+
+    this.execute = function (command) {
+        this.current = command.execute(this.current, command.value);
+        this.commands.push(command);
+        console.log(this.commands);
+    };
+
+    this.undo = function () {
+        var command = this.commands.pop();
+        this.current = command.undo(this.current, command.value);
+        console.log(this.current);
+    };
+};
+
+var calc = new Calc();
+
+// issue commands
+calc.execute(new PlusCommand(70));
+calc.execute(new MinusCommand(20));
+calc.execute(new PlusCommand(10));
+
+// reverse last two commands
+calc.undo();
+calc.undo();
