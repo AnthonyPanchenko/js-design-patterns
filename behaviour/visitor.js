ed80dcd4cb6a72b204312645829a2bc032fbc3af
Anton Panchenko <nixegend@gmail.com>
2021-10-13 00:58:09 +0300

    Visitor pattern additional implementation

--- behaviour/visitor.js
+++ behaviour/visitor.js
@@ -1,6 +1,64 @@
 // The Visitor pattern defines a new operation to a collection of objects without changing the objects themselves.
 // The new logic resides in a separate object called the Visitor.
 
+// ================================= many to many
+class Player_1 {
+  constructor(name) {
+    this.name = name;
+    this.ability = '';
+  }
+
+  accept(visitor) {
+    visitor.visit(this);
+  }
+
+  info() {
+    console.log(`I ${this.name} and i can ${this.ability}`);
+  }
+}
+
+class Player_2 {
+  constructor(name) {
+    this.name = name;
+    this.ability = '';
+  }
+
+  accept(visitor) {
+    visitor.visit(this);
+  }
+
+  info() {
+    console.log(`I ${this.name} and i can ${this.ability}`);
+  }
+}
+
+// Visitors
+class FlyVisitor {
+  visit(player) {
+    player.ability = 'fly';
+  }
+}
+
+class SwimVisitor {
+  visit(player) {
+    player.ability = 'swim';
+  }
+}
+
+const p1 = new Player_1('Player 1');
+const p2 = new Player_2('Player 2');
+
+const fVisitor = new FlyVisitor();
+const sVisitor = new SwimVisitor();
+
+p1.accept(fVisitor);
+p2.accept(sVisitor);
+
+p1.info();
+p2.info();
+
+// ====================================================== one to many
+
 class Vehicle {
   constructor(color, brand) {
     this._color = color;
